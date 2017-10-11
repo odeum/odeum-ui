cc46525057cb2dd60bc13f6da319efa7b6ad0fa3
Christian Broberg <cb@webhouse.dk>
2017-10-11 11:11:09 +0200

    Is this better

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Icon } from './components/Icon/Icon'
 export { default as Button } from './components/Button/Button'
 export { default as Modal } from './components/Modal/Modal'
+
