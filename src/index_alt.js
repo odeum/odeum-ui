3d07d66a3e61a40858df4296a137beffc9e03a32
Christian Broberg <cb@webhouse.dk>
2017-11-02 00:07:19 +0100

    Alternative import export

---
+++ src/index_alt.js
@@ -0,0 +1,25 @@
+import Icon from './components/Icon/Icon'
+import Button from './components/Button/Button'
+import Modal from './components/Modal/Modal'
+import ButtonPanel from './components/ButtonPanel/ButtonPanel'
+import ToggleSwitch from './components/ToggleSwitch/ToggleSwitch'
+
+export {
+	Icon,
+	Button,
+	Modal,
+	ButtonPanel,
+	ToggleSwitch
+}
+
+/* 
+
+export default {
+	Icon,
+	Button,
+	Modal,
+	ButtonPanel,
+	ToggleSwitch
+}
+
+*/
