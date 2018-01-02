a28afcd5ddb2756d9268842d05cca2c74fbfc77a
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2018-01-02 23:48:02 +0100

    Fixed import of styles

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyledCheckbox } from 'components/Checkbox/CheckboxStyles'
+import { StyledCheckbox } from './CheckboxStyles'
 
 class Checkbox extends Component {
 
